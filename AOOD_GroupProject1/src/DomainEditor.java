8a7e73cb75c0b762b4d5fde75a6c2f37a71d44e6
jfreem9916 <jonahwfreeman@yahoo.com>
2017-02-08 11:28:05 -0500

    help

--- AOOD_GroupProject1/src/DomainEditor.java
+++ AOOD_GroupProject1/src/DomainEditor.java
@@ -1,12 +1,31 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class DomainEditor extends JFrame {
 	private ProblemSet currentSet;
+	private JPanel panel;
+	private JTextField nameEntry, promptEntry, extraEntry, answerEntry;
+	private JButton nameSubmit, promptSubmit, extraSubmit, answerSubmit, newQu, deleteQu; 
 	public DomainEditor(){
 		setSize(800, 450);
 		setResizable(false);
 		setVisible(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		
+		panel = new JPanel();
+		nameEntry = new JTextField();
+		promptEntry = new JTextField();
+		extraEntry = new JTextField();
+		answerEntry = new JTextField();
+		nameSubmit = new JButton();
+		promptSubmit = new JButton("Enter");
+		extraSubmit = new JButton("Enter");
+		answerSubmit = new JButton("Enter");
+		newQu = new JButton("New Question");
+		deleteQu = new JButton("Delete Question");
+		
+		this.add(panel);
+		
+		
 	}
 	
 	public void loadWindow(ProblemSet ps){
