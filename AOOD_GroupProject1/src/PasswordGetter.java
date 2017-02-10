898e488ed52e1e8fbf78bb0d48e7ffbb7fcd32f2
jfreem9916 <jonahwfreeman@yahoo.com>
2017-02-10 11:53:18 -0500

    asdasd

--- AOOD_GroupProject1/src/PasswordGetter.java
+++ AOOD_GroupProject1/src/PasswordGetter.java
@@ -1,5 +1,55 @@
-import javax.swing.JPasswordField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PasswordGetter extends JPasswordField {
+import javax.swing.*;
 
+public class PasswordGetter extends JFrame {
+	private JPanel panel;
+	private JPasswordField passField;
+	private JLabel prompt;
+	private JButton enter;
+	private JButton cancel;
+	private StudyHelper sh;
+	public PasswordGetter(StudyHelper s){
+		sh = s;
+		setVisible(true);
+		setResizable(false);
+		setSize(300, 100);
+		enter = new JButton("OK");
+		cancel = new JButton("Cancel");
+		setLocation(800-150, 450-50);
+		add(panel);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+		panel.add(prompt);
+		panel.add(passField);
+		panel.add(enter);
+		panel.add(cancel);
+		enter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				
+				
+			}
+		});
+		cancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				setVisible(false);
+				
+				
+			}
+		});
+	}
+	
+	public String getPass(){
+		char[] out = passField.getPassword();
+		passField.setText("");
+		String pass = String.copyValueOf(out);
+		out = null;
+		
+		return pass;
+	}
+	
 }
